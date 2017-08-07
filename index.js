e00f4010d42266d4b5619f9a1a5a718c7b59b2d6
Oksana <info@iti-digital.com>
2017-08-06 22:20:43 -0700

    added files

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('test');
+var XLSX = require('xlsx');
+
+function excel_to_json(excelPath) {
+    var json_sheet = [];
+    var workbook = XLSX.readFile(excelPath);
+    var sheets = workbook.SheetNames;
+    for (var i = 0; i < sheets.length; i++) {
+        var ws;
+        try {
+            ws = workbook.Sheets[sheets[i]];
+            json_sheet = XLSX.utils.sheet_to_json(ws);
+        } catch (e) {
+            console.error('error parsing: ' + e);
+        }
+    }
+    return json_sheet;
+}
+module.exports = excel_to_json;
